dca584d47502925e829b7667a6cb252585af1728
Silchas <Cartharoncrust@gmail.com>
2023-06-21 18:36:22 +0300

    some changes to authentication

--- src/components/TodoForm.jsx
+++ src/components/TodoForm.jsx
@@ -1,6 +1,7 @@
-import { useState } from "react";
+import { useContext, useState } from "react";
 import { useTodoContext } from "../context/TodoProvider";
 import AuthForm from "./Auth";
+import { SignInContext } from "../context/AuthContext";
 
 const TodoForm = () => {
     const { todoList, addTask, useToggle } = useTodoContext();
@@ -19,10 +20,23 @@ if(!value){
     setValue('')
     console.log("new task=>", value);
     };
+
+    const handleLogout = ()=>{
+        localStorage.removeItem('Email')
+        window.location.reload()
+    }
+    const {currentUser} = useContext(SignInContext)
     return (
-    <form className="" onSubmit={submit}>
+        <>
+
+{currentUser === null ?
+            <AuthForm/> 
+            : 
+            <>
+                        <button onClick={handleLogout}>Logout</button>
+        <form className="" onSubmit={submit}>
         <h3>To Do Items: {todoList.length}</h3>
-        <AuthForm/>
+            
         <input className="m-3 placeholder:italic placeholder:text-slate-400 border border-slate-300 rounded-md py-2 pl-9 pr-3 shadow-sm focus:outline-none focus:border-sky-500 focus:ring-sky-500 focus:ring-1 sm:text-sm" placeholder="Add Task"
         type="text"
         value={value}
@@ -32,6 +46,14 @@ if(!value){
         
         
     </form>
+            </>
+
+    }
+        </>
+
+
+        
+
     );
 };
 
